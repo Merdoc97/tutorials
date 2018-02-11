af25cf33e7e7b3696f444633b08b889f56a53d31
Igor <merdoc97@gmail.com>
2018-02-11 23:38:21 +0200

    embedded elastic finish

---
+++ test_containers_examples/hibernate-search/service/src/test/java/com/github/examples/config/TestConfiguration.java
@@ -0,0 +1,29 @@
+package com.github.examples.config;
+
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.GenericContainer;
+
+/**
+ * Created on 09.01.2017.
+ * imbeded postgres sql for tests only
+ */
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = {TestDataSourceConfig.class, JpaConfig.class, BeanConfiguration.class})
+public class TestConfiguration {
+
+    @Value("${elastic.port}")
+    private Integer elasticPort;
+
+    @ClassRule
+    public static GenericContainer container = ElasticSearchEmbeddedConfiguration.elasticEmbedded(9201);
+
+
+}
--- test_containers_examples/test-configs/pg_embedded/src/main/java/com/github/examples/config/PgEmbeddedConfiguration.java
+++ test_containers_examples/test-configs/pg_embedded/src/main/java/com/github/examples/config/PgEmbeddedConfiguration.java
@@ -2,20 +2,15 @@ package com.github.examples.config;
 
 import com.opentable.db.postgres.embedded.EmbeddedPostgres;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 import java.io.IOException;
 
 /**
- * Created on 09.01.2017.
- * imbeded postgres sql for tests only
+ * if you want use pg embedded configuration just inherit current class and add annotation @Configuration
  */
-@Profile("test")
-@Configuration
-public class TestDataSource {
+public class PgEmbeddedConfiguration {
 
     @Bean
     @Primary
